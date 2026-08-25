1ba43e191567ef581733f291a8a891ea823ace72
J1R4T <mzkanryt2547@gmail.com>
2026-08-25 22:59:32 +0700

    Delete Nuutapao-Tools-v3.1.1 directory

--- Nuutapao-Tools-v3.1.1/playwright.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  testDir: './test/ui',
-  timeout: 30_000,
-  retries: 0,
-  reporter: 'list',
-  use: { trace: 'retain-on-failure' }
-});
